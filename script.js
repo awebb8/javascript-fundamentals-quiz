c3538ea14ff2d46cf4f9cd8bea56787d0c8ea3dd
Alkawe <alixkwebb@gmail.com>
2020-09-07 10:57:25 -0400

    added an Object called questionObject to hold all questions and answers

--- script.js
+++ script.js
@@ -3,6 +3,19 @@
     // Add event listener to Start button.
 
 // Once the Start button is clicked, run the a timer function.  Also initiate the first question.
+var questionObject = {
+    "Commonly used data types DO NOT include:": "alerts",
+    "The condition in an if / else statement is enclosed within ________.": "parentheses",
+    "Arrays in JavaScript can be used to store ________.": "all of the above",
+    "String values must be enclosed within ________ when being assigned to variables.": "quotes",
+    "A very useful tool used during development and debugging for printing content to the debugger is:": "console log"
+};
+
+console.log(questionObject["Commonly used data types DO NOT include:"]);
+console.log(questionObject["The condition in an if / else statement is enclosed within ________."]);
+console.log(questionObject["Arrays in JavaScript can be used to store ________."]);
+console.log(questionObject["String values must be enclosed within ________ when being assigned to variables."]);
+console.log(questionObject["A very useful tool used during development and debugging for printing content to the debugger is:"]);
 
     // If the answer is correct, present the second question.
 
@@ -14,4 +27,3 @@
 
 // After the game is over, save the player's initials and score.
 
-fef
