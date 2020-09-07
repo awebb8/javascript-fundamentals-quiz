be37eed35e9a53fa73cbe633db095f620dd3e382
Alkawe <alixkwebb@gmail.com>
2020-09-06 21:50:06 -0400

    added pseudocode

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Clicking the Start button initiates the quiz.
+    // Add event listener to Start button.
+
+// Once the Start button is clicked, run the a timer function.  Also initiate the first question.
+
+    // If the answer is correct, present the second question.
+
+    // If the answer is incorrect, subtract time from the clock.
+
+    // If all questions are answered, the game is over.
+
+    // If the timer reaches 0, the game is over.
+
+// After the game is over, save the player's initials and score.
+
+fef
