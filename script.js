8d7a727cafe27ba9e21b75ee6b4b1257ec6a33c0
Alkawe <alixkwebb@gmail.com>
2020-09-09 18:37:48 -0400

    added timer function and changed the for loop to the iterate function

--- script.js
+++ script.js
@@ -12,10 +12,6 @@ var answerTwoBtn = document.querySelector("#answerTwo");
 var answerThreeBtn = document.querySelector("#answerThree");
 var answerFourBtn = document.querySelector("#answerFour");
 
-console.log(answerOneBtn.textContent);
-console.log(answerTwoBtn.textContent);
-console.log(answerThreeBtn.textContent);
-console.log(answerFourBtn.textContent);
 
     // Clicking the Start button initiates the quiz.
     // Add event listener to Start button.
@@ -32,170 +28,199 @@ var questionObjectsArray = [
     },
     {
         question: "The condition in an if / else statement is enclosed within ________.",
-        Answer1: "quotes",
-        Answer2: "curly brackets",
-        Answer3: "parentheses",
-        Answer4: "square brackets",
-        correctAnswer: "parentheses"
+        Answer1: "1. quotes",
+        Answer2: "2. curly brackets",
+        Answer3: "3. parentheses",
+        Answer4: "4. square brackets",
+        correctAnswer: "3. parentheses"
     },
     {
         question:  "Arrays in JavaScript can be used to store ________.",
-        Answer1: "numbers and strings",
-        Answer2: "other arrays",
-        Answer3: "booleans",
-        Answer4: "all of the above",
-        correctAnswer: "all of the above"
+        Answer1: "1. numbers and strings",
+        Answer2: "2. other arrays",
+        Answer3: "3. booleans",
+        Answer4: "4. all of the above",
+        correctAnswer: "4. all of the above"
     },
     {
         question: "String values must be enclosed within ________ when being assigned to variables.",
-        Answer1: "commas",
-        Answer2: "curly brackets",
-        Answer3: "quotes",
-        answer4: "parentheses",
-        correctAnswer: "quotes"
+        Answer1: "1. commas",
+        Answer2: "2. curly brackets",
+        Answer3: "3. quotes",
+        Answer4: "4. parentheses",
+        correctAnswer: "3. quotes"
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        Answer1: "JavaScript",
-        Answer2: "terminal/bash",
-        Answer3: "for loops",
-        Answer4: "console log",
-        correctAnswer: "console log"
+        Answer1: "1. JavaScript",
+        Answer2: "2. terminal/bash",
+        Answer3: "3. for loops",
+        Answer4: "4. console log",
+        correctAnswer: "4. console log"
+    },
+    {
+        question: "calculating score",
+        Answer1: "calculating score",
+        Answer2: "calculating score",
+        Answer3: "calculating score",
+        Answer4: "calculating score",
+        correctAnswer: "calculating score"
     }
 ];
 
-// correct: function() {
-//     alert("Correct!");
-    // Initiate function to diplay a new question
-// }
 
-    // incorrect: function() {
-    //     alert("Wrong");
-    // }
+// Set variables for the timer
+var timerEl = $("#timer");
+var timeLeft = 120;
+console.log(timerEl);
 
-// function startTimer() {
-//     // Code to start the timer
-//     console.log(quizSeconds.value);
-//     var timeInSeconds = parseInt(quizSeconds.value);
-//     console.log(timeInSeconds);
-//     clearInterval(interval);
-//     interval = setInterval( function () {
-//         console.log("interval is running");
-//         console.log("current timeInSeconds", imeInSeconds);
-//         timeInSeconds--;
-//         console.log("New TimeInSeconds", timeInSeconds);
-//     }, 1000)
-// }
+// Create function for the timer
+function setTimer() {
 
-// var functionsArray [
-//     {
+    var timeInterval = setInterval(function() {
+        timeLeft--;
+        timerEl.text(timeLeft);
+        
+  
+        if (timeLeft === 0 || questionIndex >= 6) {
+            timerEl.textContent = "";
+            clearInterval(timeInterval);
+            timerEl.textContent = 0;
+            alert("You ran out of time");
+        
+      }
+    }, 1000);
+  }
 
-//     }
-// ];
 
-console.log(questionObjectsArray[0].question);
-console.log(questionObjectsArray[0].correctAnswer);
-// console.log(questionObject["The condition in an if / else statement is enclosed within ________."]);
-// console.log(questionObject["Arrays in JavaScript can be used to store ________."]);
-// console.log(questionObject["String values must be enclosed within ________ when being assigned to variables."]);
-// console.log(questionObject["A very useful tool used during development and debugging for printing content to the debugger is:"]);
 
-// First page; Intro
+// First page; Intro & Start Button
 var startBtn = document.querySelector("#start-button");
 startBtn.addEventListener("click", startBtnFunction);
 
 function startBtnFunction() {
     // Empty/clear the intro
-    $("#entire-intro-card").empty();
+    // $("#entire-intro-card").empty();
+    $("#question-section").addClass("display");
+
+    $("#entire-intro-card").addClass("hide");
     
     // TODO: Start the timer
-    
+    setTimer();
+
+    iterate();
 }
 
-// function questionsLoop() {
+var questionIndex = 0;
+
 // Loop through all the questions, changing the question & answers each time.
-for (var i = 0; i < 5; i++) {
+function iterate() {
+
+    numberRight = 0;
+
+    if (this.textContent === undefined) {
+        console.log(this.text);
+    }
+    else if (this.textContent === questionObjectsArray[questionIndex - 1].correctAnswer) {
+        footerResult.textContent = "Correct!";
+        numberRight++;
+        console.log(numberRight);
+    }
+
+    console.log(this.textContent);
     // Use jQuery code to empty/clear the div
     // var cardElement = document.getElementById("#entire-Card");
     // $("#question-header").empty();
     
     // Display the question
-    questionHeader.textContent = questionObjectsArray[i].question;
-    console.log(questionObjectsArray[i].question);
+    questionHeader.textContent = questionObjectsArray[questionIndex].question;
 
     // Display the answer choices
-    answerOneBtn.textContent = questionObjectsArray[0].Answer1;
-    answerTwoBtn.textContent = questionObjectsArray[0].Answer2;
-    answerThreeBtn.textContent = questionObjectsArray[0].Answer3;
-    answerFourBtn.textContent = questionObjectsArray[0].Answer4;
+    answerOneBtn.textContent = questionObjectsArray[questionIndex].Answer1;
+    answerTwoBtn.textContent = questionObjectsArray[questionIndex].Answer2;
+    answerThreeBtn.textContent = questionObjectsArray[questionIndex].Answer3;
+    answerFourBtn.textContent = questionObjectsArray[questionIndex].Answer4;
 
-    // When an answer button is clicked, run function to determine correct or incorrect
-    answerOneBtn.addEventListener("click", answerOneBtnFunction);
-    answerTwoBtn.addEventListener("click", answerTwoBtnFunction);
-    answerThreeBtn.addEventListener("click", answerThreeBtnFunction);
-    answerFourBtn.addEventListener("click", answerFourBtnFunction);
+    questionIndex += 1;
+
 
 }
-// }
 
 // Variable for counting the number of correct answers
-var numberRight = 0;
+// var numberRight = 0;
 
-function answerOneBtnFunction() {
-    // If the answer is correct, alert "Correct!"
-    if (answerOneBtn.textContent == questionObjectsArray[0].correctAnswer) {
-        // questionObjectsArray.correct();
-        footerResult.textContent = "Correct!";
-        numberRight++;
-        console.log(numberRight);
-    }
-    // If the answer is incorrect, display "Wrong" and subtract time from the clock.
-    else if (answerOneBtn.textContent !== questionObjectsArray[0].correctAnswer) {
-    //     questionObjectsArray.incorrect();
-        footerResult.textContent = "Wrong";
-    }
-}
-    // return numberRight;
-function answerTwoBtnFunction() {
-    // If the answer is correct, alert "Correct!"
-    if (answerTwoBtn.textContent == questionObjectsArray[0].correctAnswer) {
-        footerResult.textContent = "Correct!";
-        numberRight++;
-        console.log(numberRight);
-    }
-    // If the answer is incorrect, display "Wrong" and subtract time from the clock.
-    else if (answerTwoBtn.textContent !== questionObjectsArray[0].correctAnswer) {
-        footerResult.textContent = "Wrong";
-    }
-}
-function answerThreeBtnFunction() {
-    // If the answer is correct, alert "Correct!"
-    if (answerThreeBtn.textContent == questionObjectsArray[0].correctAnswer) {
-        footerResult.textContent = "Correct!";
-        numberRight++;
-        console.log(numberRight);
-    }
-    // If the answer is incorrect, display "Wrong" and subtract time from the clock.
-    else if (answerThreeBtn.textContent !== questionObjectsArray[0].correctAnswer) {
-        footerResult.textContent = "Wrong";
-    }
-}
-function answerFourBtnFunction() {
-    // If the answer is correct, alert "Correct!"
-    if (answerFourBtn.textContent == questionObjectsArray[0].correctAnswer) {
-        footerResult.textContent = "Correct!";
-        numberRight++;
-        console.log(numberRight);
-    }
-    // If the answer is incorrect, display "Wrong" and subtract time from the clock.
-    else if (answerFourBtn.textContent !== questionObjectsArray[0].correctAnswer) {
-        footerResult.textContent = "Wrong";
-    }
-}
-
+// function answerOneBtnFunction() {
+//     // If the answer is correct, alert "Correct!"
+//     if (answerOneBtn.textContent == questionObjectsArray[i].correctAnswer) {
+//         // questionObjectsArray.correct();
+//         footerResult.textContent = "Correct!";
+//         numberRight++;
+//         console.log(numberRight);
+//     }
+//     // If the answer is incorrect, display "Wrong" and subtract time from the clock.
+//     else if (answerOneBtn.textContent !== questionObjectsArray[i].correctAnswer) {
+//     //     questionObjectsArray.incorrect();
+//         footerResult.textContent = "Wrong";
+//     }
+// }
+//     // return numberRight;
+// function answerTwoBtnFunction() {
+//     // If the answer is correct, alert "Correct!"
+//     if (answerTwoBtn.textContent == questionObjectsArray[i].correctAnswer) {
+//         footerResult.textContent = "Correct!";
+//         numberRight++;
+//         console.log(numberRight);
+//     }
+//     // If the answer is incorrect, display "Wrong" and subtract time from the clock.
+//     else if (answerTwoBtn.textContent !== questionObjectsArray[i].correctAnswer) {
+//         footerResult.textContent = "Wrong";
+//     }
+// }
+// function answerThreeBtnFunction() {
+//     // If the answer is correct, alert "Correct!"
+//     if (answerThreeBtn.textContent == questionObjectsArray[i].correctAnswer) {
+//         footerResult.textContent = "Correct!";
+//         numberRight++;
+//         console.log(numberRight);
+//     }
+//     // If the answer is incorrect, display "Wrong" and subtract time from the clock.
+//     else if (answerThreeBtn.textContent !== questionObjectsArray[i].correctAnswer) {
+//         footerResult.textContent = "Wrong";
+//     }
+// }
+// function answerFourBtnFunction() {
+//     // If the answer is correct, alert "Correct!"
+//     if (answerFourBtn.textContent == questionObjectsArray[i].correctAnswer) {
+//         footerResult.textContent = "Correct!";
+//         numberRight++;
+//         console.log(numberRight);
+//     }
+//     // If the answer is incorrect, display "Wrong" and subtract time from the clock.
+//     else if (answerFourBtn.textContent !== questionObjectsArray[i].correctAnswer) {
+//         footerResult.textContent = "Wrong";
+//     }
+// }
+// }
     // If all questions are answered, the game is over.
     
     // If the timer reaches 0, the game is over.
 
 // After the game is over, save the player's initials and score.
+
 // alert("Number correct: " + answerLog());
+
+    // When an answer button is clicked, run function to determine correct or incorrect
+    answerOneBtn.addEventListener("click", iterate);
+    answerTwoBtn.addEventListener("click", iterate);
+    answerThreeBtn.addEventListener("click", iterate);
+    answerFourBtn.addEventListener("click", iterate);
+
+
+function displayResults () {
+
+}
+
+var submitBtn = document.querySelector("#submit-button");
+
+submitBtn.addEventListener('click', displayResults);
+
